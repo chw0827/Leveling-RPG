68acafe502aa04bf64fe60d3b0f2c9c36b0a5191
chw0827 <92154256+chw0827@users.noreply.github.com>
2024-04-29 15:49:17 +0900

    1

---
+++ Leveling RPG/Assets/Scripts/GameManager.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+
+    public static GameManager Instance
+    {
+        get
+        {
+            if (!instance)
+            {
+                instance = FindObjectOfType(typeof(GameManager)) as GameManager;
+            }
+            return instance;
+        } 
+    }
+
+    private void Awake()
+    {
+        if (instance == null)
+            instance = this;
+
+        else if (instance != this)
+            Destroy(gameObject);
+
+        DontDestroyOnLoad(gameObject);
+    }
+}
