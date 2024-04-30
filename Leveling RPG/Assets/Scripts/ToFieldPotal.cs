9521c56931a714bb1dcbd01d2fc124e682a78411
chw0827 <92154256+chw0827@users.noreply.github.com>
2024-04-30 17:23:22 +0900

    1

--- Leveling RPG/Assets/Scripts/ToFieldPotal.cs
+++ Leveling RPG/Assets/Scripts/ToFieldPotal.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class Potal : MonoBehaviour
+public class ToFieldPotal : MonoBehaviour
 {
     private void OnTriggerEnter(Collider other)
     {
